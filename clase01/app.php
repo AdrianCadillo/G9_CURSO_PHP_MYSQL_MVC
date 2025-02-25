9a0b069a43c15743a9d93bad7d4b0cebdd140d80
AdrianFullStack <Adrian@tecnologySoft.com>
2025-02-24 21:14:21 -0500

    clase 01

---
+++ clase01/app.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <input type="text" value="<?php echo "Hola mundo!!!" ?>">
+    <form action="">
+        <input type="text" name="username">
+        <input type="text" name="password">
+        <button>Guardar</button>
+    </form>
+</body>
+</html>
